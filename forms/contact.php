a37182206c150049d82f83bf3da557aa81c1689a
jenish798 <solomonjenish@gmail.com>
2025-01-09 22:32:43 +0530

    form

---
+++ forms/contact.php
@@ -0,0 +1,81 @@
+<?php
+require 'mailer/src/Exception.php';
+require 'mailer/src/PHPMailer.php';
+require 'mailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Collect form data
+// $to = ["training@rsrglobal.org"];
+$to = ["solomonjenish@gmail.com"];
+$from = $_POST['email'];
+$name = $_POST['name'];
+$contact_number = $_POST['contact_number'];
+$qualification = $_POST['qualification'];
+$city = $_POST['city'];
+$country = $_POST['country'];
+$program = $_POST['program'];
+$destination = $_POST['destination'];
+$message = $_POST['message'];
+$sub = 'Education Enquiry';
+$sub1 = 'Education Enquiry';
+
+$message = "
+<b>Name:</b> $name<br>
+<b>Contact Number:</b> $contact_number<br>
+<b>Email Id:</b> $from<br>
+<b>qualification:</b> $qualification<br>
+<b>city:</b> $city<br>
+<b>country:</b> $country<br>
+<b>program:</b> $program<br>
+<b>destination:</b> $destination<br>
+<b>Message:</b> $message
+";
+
+try {
+    $mail = new PHPMailer(true);
+
+    // Server settings
+    // $mail->SMTPDebug = 2; // For detailed debugging info
+    $mail->isSMTP();                                   // Set mailer to use SMTP
+    $mail->Host       = 'smtp.office365.com';         // Specify main SMTP server
+    $mail->SMTPAuth   = true;                         // Enable SMTP authentication
+    $mail->Username   = 'noreply@rsrglobal.org';      // SMTP username
+    $mail->Password   = 'RSRGlobal@2023';             // SMTP password (use an App Password if 2FA is enabled)
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption
+    $mail->Port       = 587;                          // TCP port to connect to
+
+    // Send email to recipients
+    $mail->setFrom('noreply@rsrglobal.org', 'RSR Global');
+    foreach ($to as $recipient) {
+        $mail->addAddress($recipient); // Add recipient
+    }
+    $mail->addReplyTo('contact@rsrglobal.org', 'RSR Global');
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $sub;
+    $mail->Body    = $message;
+    $mail->AltBody = strip_tags($message);
+
+    $mail->send();
+
+    // Send confirmation email
+    $confirmation_message = "
+    <p>Dear $name,</p>
+    <p>Thank you for your enquiry. We will review your information and get back to you shortly.</p>
+    <p>Best regards,<br>RSR Global Education</p>
+    ";
+
+    $mail->clearAddresses(); // Clear previous recipients
+    $mail->addAddress($from); // Add the sender as recipient
+    $mail->Subject = $sub1;
+    $mail->Body    = $confirmation_message;
+    $mail->AltBody = strip_tags($confirmation_message);
+
+    $mail->send();
+    echo 'ok';
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
---
+++ js/contactvalidate.js
@@ -0,0 +1,114 @@
+
+(function () {
+    "use strict";
+  
+    let forms = document.querySelectorAll('.enquiry-form');
+  
+    forms.forEach( function(e) {
+      e.addEventListener('submit', function(event) {
+        event.preventDefault();
+  
+        let thisForm = this;
+  
+        let action = thisForm.getAttribute('action');
+        let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+        
+        if( ! action ) {
+          displayError(thisForm, 'The form action property is not set!');
+          return;
+        }
+        thisForm.querySelector('.loading').classList.add('d-block');
+        thisForm.querySelector('.error-message').classList.remove('d-block');
+        thisForm.querySelector('.sent-message').classList.remove('d-block');
+  
+        // let formData = new FormData( thisForm );
+  
+        var formData = {
+          name: document.getElementById('name').value,
+          email: document.getElementById('email').value,
+          contact_number: document.getElementById('contact_number').value,
+          qualification: document.getElementById('qualification').value,
+          city: document.getElementById('city').value,
+          country: document.getElementById('country').value,
+          program: document.getElementById('program').value,
+          destination: document.getElementById('destination').value,
+          message: document.getElementById('message').value,
+        };
+  
+        // var countryCode = country.phone;
+  
+        // var concatenatedPhoneNumber = countryCode + phone;
+        
+        console.log(formData);
+  
+        if ( recaptcha ) {
+          if(typeof grecaptcha !== "undefined" ) {
+            grecaptcha.ready(function() {
+              try {
+                grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+                .then(token => {
+                  formData.set('recaptcha-response', token);
+                  php_email_form_submit(thisForm, action, formData);
+                })
+              } catch(error) {
+                displayError(thisForm, error);
+              }
+            });
+          } else {
+            displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+          }
+        } else {
+          php_email_form_submit(thisForm, action, formData);
+        }
+      });
+    });
+  
+    function php_email_form_submit(thisForm, action, formData) {
+      console.log(formData);
+        var form_data = new FormData();
+    
+      for ( var key in formData ) {
+          form_data.append(key, formData[key]);
+      }
+      console.log(form_data);
+      fetch(action, {
+        method: 'POST',
+        body: form_data,
+        headers: {'X-Requested-With': 'XMLHttpRequest'}
+      })
+      .then(response => {
+        console.log(response);
+        if( response.ok ) {
+          return response.text();
+        } else {
+          console.log('error');
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+        }
+      })
+      .then(data => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        console.log(data);
+        if (data == 'ok') {
+          console.log('click');
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+          setTimeout(()=>{
+            thisForm.querySelector('.sent-message').classList.remove('d-block'); 
+          },3000) 
+        } else {
+          // throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, error);
+      });
+    }
+  
+    function displayError(thisForm, error) {
+      thisForm.querySelector('.loading').classList.remove('d-block');
+      thisForm.querySelector('.error-message').innerHTML = error;
+      thisForm.querySelector('.error-message').classList.add('d-block');
+    }
+  
+  })();
+  
